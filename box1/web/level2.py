df22d000c7b0a2c83b098a7943a311392f2eaef7
A Yeep <yeepyeepyeep@gmail.com>
2017-02-13 21:32:55 +0000

    Added a directory listing exploit
    Added a gallery page that lists from a path.

--- box1/web/level2.py
+++ box1/web/level2.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from os import listdir
+from os.path import isfile, join
 import socket
 
 app = Flask(__name__)
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('level2/index.html')
+
+@app.route('/gallery')
+def gallery():
+    path = join('static', 'pictures', request.args.get('path'))
+    images = [f for f in listdir(path) if (isfile(join(path, f)))]
+    return render_template('level2/gallery.html', path=path, images=images)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1338)
