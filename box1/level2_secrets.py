a73472d771073981817843ddb2695c2337779e93
A Yeep <yeepyeepyeep@gmail.com>
2017-03-17 08:50:10 +0000

    Create secrets directory

--- box1/level2_secrets.py
+++ box1/level2_secrets.py
@@ -1,5 +1,9 @@
 import random
 import string
+import os
+
+if not os.path.exists('web/secrets'):
+    os.makedirs('web/secrets')
 
 with open('web/secrets/password', "wt") as f:
     f.write(''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(20)))
